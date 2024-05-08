dd7ef45019c3e290c3010fb2c63f7b4380adc9a3
MaxAnii <ansarbaba2000@gmail.com>
2024-05-08 18:25:55 +0530

    feat: add a page to regenerate the password.

---
+++ src/pages/ResetPassword.tsx
@@ -0,0 +1,13 @@
+import GenerateNewPassword from "@/components/GenerateNewPassword";
+import TokenVerification from "@/components/TokenVerification";
+
+const ResetPassword = () => {
+	return (
+		<>
+			{/* <TokenVerification></TokenVerification> */}
+			<GenerateNewPassword></GenerateNewPassword>
+		</>
+	);
+};
+
+export default ResetPassword;
