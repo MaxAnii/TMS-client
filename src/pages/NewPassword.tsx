89326b27a3ac523ac7ec79a006186c5ddad54110
MaxAnii <ansarbaba2000@gmail.com>
2024-05-23 11:47:19 +0530

    feat: add a page to create new password.

---
+++ src/pages/NewPassword.tsx
@@ -0,0 +1,10 @@
+import NewPasswordForm from "@/components/newPasswordForm";
+const NewPassword = () => {
+	return (
+		<div>
+			<NewPasswordForm></NewPasswordForm>
+		</div>
+	);
+};
+
+export default NewPassword;
