d27ab106f3ab4ede1998ab468c335319f737ee83
MaxAnii <ansarbaba2000@gmail.com>
2024-05-23 11:43:58 +0530

    feat: add a schema to reset the password.

--- schema/index.ts
+++ schema/index.ts
@@ -9,6 +9,14 @@ export const resetLinkSchema = z.object({
 	email: z.string().min(5),
 });
 
+export const newPasswordSchema = z
+	.object({
+		password: z.string().min(5),
+		confirmPassword: z.string().min(5),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "Passwords must match",
+	});
 export const deviceDataSchema = z.object({
 	modelNumber: z.string().min(2),
 	vehicleNumber: z.string().min(2),
