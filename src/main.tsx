e5bfc0db56b724b102fa71d79ece7813671484af
MaxAnii <ansarbaba2000@gmail.com>
2024-05-24 12:40:49 +0530

    feat: wrap the app with the user context.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import UserSessionProvider from "./contextHook/CurrentUserSession.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<UserSessionProvider>
+				<App />
+			</UserSessionProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
